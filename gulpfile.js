8d4e0a56f82e99804b9d0784e17fc05ce3ad7942
Kseniya Vasileva <vasex.xena@ya.ru>
2020-05-07 00:46:16 +0500

    feat: install gulp-autoprefixer

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ const browserSync = require('browser-sync').create();
 const cssmin = require('gulp-cssmin');
 const rename = require('gulp-rename');
 const sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
 
 
 // Static server
@@ -29,6 +30,9 @@ function bs() {
 function serveSass () {
 	return src('./src/sass/*.sass')
 			.pipe(sass())
+			.pipe(autoprefixer({
+				cascade: false
+			}))
 			.pipe(dest("./src/css"))
 			.pipe(browserSync.stream());
 };
